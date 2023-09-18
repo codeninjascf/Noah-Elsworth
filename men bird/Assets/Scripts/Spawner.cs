00db646b743520257d4a47006298a6a892d98bfe
codeninjascf <cnchandlersford@gmail.com>
2023-09-18 16:16:10 +0100

    menny bird

--- men bird/Assets/Scripts/Spawner.cs
+++ men bird/Assets/Scripts/Spawner.cs
@@ -7,12 +7,27 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //Start function repeating every 4 seconds
+        InvokeRepeating("InstantiateObjects", 1f, 4f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Position for the gameobjects
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
     }
+}
+//Object that we will attach to the script for spawning object
+[Header("Spikes Object for controlling the game")]
+public GameObject spikes;
+//Height position of the spikes
+[Header("Default Height")]
+public float height;
+
+//InstantiateObjects Function
+void InsantiateObjects()
+{
+    //Spawn object by position and rotation
+    Insantiate(spikes, transform.position, transform.rotation);
 }
