beabe051feb004496a49fe9b7d17255dcbaf9ad4
codeninjascf <cnchandlersford@gmail.com>
2023-10-30 16:14:35 +0000

    jometry dash

--- jom jom dash/Assets/Scripts/Spawner.cs
+++ jom jom dash/Assets/Scripts/Spawner.cs
@@ -9,6 +9,7 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _activeObjects = new List<GameObject>();
         
         StartCoroutine(Spawn());
     }
