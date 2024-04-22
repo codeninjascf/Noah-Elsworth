fb18b2b62365bf6477dfc122250d6c2fd9798068
codeninjascf <cnchandlersford@gmail.com>
2024-04-22 16:11:03 +0100

    honk honk

---
+++ My project (3)/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Level level;
+    public GridManager gridManager;
+
+    private bool _gameEnded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameEnded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(level.GameOver &&)
+    }
+}
--- My project (3)/Assets/Scripts/Level.cs
+++ My project (3)/Assets/Scripts/Level.cs
@@ -29,14 +29,27 @@ public class Level : MonoBehaviour
         || type == LevelType.Time && TimeRemaining <= 0;
     public int StarsAchieved(int score) => Convert.ToInt32(score >= firstStar) +
         Convert.ToInt32(score >= secondStar) + Convert.ToInt32(score >= thirdStar);
+    public void UpdateHighScore(int score) => PlayerPrefs.SetInt(_levelName +
+    "Highscore", Math.Max(score, HighScore));
+
+    public void UpdateStarsAchieved(int score) => PlayerPrefs.SetInt(_levelName +
+    "_Stars", StarsAchieved(Score));
     void Start()
     {
         
     }
+    void Awake()
+    {
+        _levelName = SceneManager.GetActiveScene().name;
 
+        MovesRemaining = levelCondition;
+        TimeRemaining = levelCondition;
+
+        HighScore = PlayPrefs.GetInt(_levelName + "_Highscore", 0);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        TimeRemaining -= TimeRemaining.deltaTime;
     }
 }
