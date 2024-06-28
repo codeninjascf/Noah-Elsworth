798c06f44d363e9c15aa4803e77495a7d3c1f10b
codeninjascf <cnchandlersford@gmail.com>
2024-06-28 16:05:36 +0100

    game manager in slot thing

--- amaz/Assets/Scripts/PlayerController.cs
+++ amaz/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameManager gameManager;
     public float moveSpeed = 5f;
     public float jumpforce = 10f;
 
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour
     public LayerMask whatIsGround;
 
     private bool _isGrounded;
+    private bool _enabled;
     private Rigidbody2D _rigidbody;
     // Start is called before the first frame update
     void Start()
@@ -22,6 +24,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!_enabled) return;
         _isGrounded = Physics2D.Raycast(transform.position, Vector2.down,
             groundDistanceThreshold, whatIsGround);
 
@@ -30,10 +33,29 @@ public class PlayerController : MonoBehaviour
             _rigidbody.velocity = Vector2.up * jumpforce;
         }
     }
-    private void FixedUpdate()
+    void FixedUpdate()
     {
+        if (!_enabled) return;
         float movement = moveSpeed * Input.GetAxisRaw("Horizontal");
 
         _rigidbody.position += movement * Time.deltaTime * Vector2.right;
+    }
+
+    public void Enable()
+    {
+        _enabled = true;
+    }
+
+    public void Disable()
+    {
+        _enabled = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            gameManager.KillPlayer();
+        }
     }
 }
